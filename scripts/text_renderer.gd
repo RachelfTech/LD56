11281d5b4bdd34cd28c5fdca5050abf83394cc81
RachelfTech <129456439+RachelfTech@users.noreply.github.com>
2024-10-05 00:56:42 -0400

    Add basic text audio

--- scripts/text_renderer.gd
+++ scripts/text_renderer.gd
@@ -4,10 +4,11 @@ extends CanvasLayer
 
 @export var text_label: RichTextLabel
 @export var text_container: MarginContainer
+@export var audio_player: AudioStreamPlayer
 
 
 var scene_text_strings: Array[String] = [
-    "[center]One day I woke up and you had [wave]changed....[/wave][/center]",
+    "[center]I woke up one morning and you had [wave]changed....[/wave][/center]",
 ]
 
 var current_text: String = scene_text_strings[0]
@@ -24,13 +25,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     pass
 
+func animate_in():
+    audio_player.play()
 
-func fade_in():
     text_container.modulate = Color.TRANSPARENT
     var tween: Tween = get_tree().create_tween()
     tween.tween_property(text_container, "modulate", Color.WHITE, 1.5).set_trans(Tween.TRANS_SINE)
     tween.set_parallel()
     tween.tween_property(text_label, "visible_ratio", 1, 2).set_trans(Tween.TRANS_LINEAR)
-
-func _animate_text():
-    text_label.visible_ratio = 0.5
