3ebe012587176556c9691357b56b40c558b5aea8
RachelfTech <129456439+RachelfTech@users.noreply.github.com>
2024-10-04 23:57:13 -0400

    Opening scene

---
+++ scripts/text_renderer.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@export var text_label: RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass
+   # text_label.visible_ratio = 0.5
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
