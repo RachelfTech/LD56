c095af9015ed80bb5b71155b191cb1510a091338
RachelfTech <129456439+RachelfTech@users.noreply.github.com>
2024-10-06 18:13:00 -0400

    Add audio

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,6 +7,10 @@ var current_sequence: Sequence
 var current_sequence_index: int = 0
 var sequence_finished: bool = false
 
+@export var main_theme: AudioStreamPlayer
+@export var ominious: AudioStreamPlayer
+@export var end_theme: AudioStreamPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     _start_sequence(sequences_scenes[current_sequence_index])
@@ -19,12 +23,23 @@ func _start_next_sequence():
     current_sequence_index += 1
     if current_sequence_index >= sequences_scenes.size():
         print("finished all sequences")
+        end_theme.stop()
+        main_theme.play()
         current_sequence_index = 0
     var next_sequence: PackedScene = sequences_scenes[current_sequence_index]
 
     # Disconnect the end signal so it can't fire before the sequence is queue_free'd.
     current_sequence.finished.disconnect(_handle_sequence_finished)
     current_sequence.queue_free()
+
+    if current_sequence_index == 6:
+        main_theme.stop()
+        ominious.play()
+
+    if current_sequence_index == 7:
+        main_theme.stop()
+        ominious.stop()
+        end_theme.play()
 
     _start_sequence(next_sequence)
 
